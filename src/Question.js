5e0671b175bf5e7efabc6d9d6879223abab82b73
Swathi Polapally <37867151+shalini-polapally@users.noreply.github.com>
2023-06-26 20:30:35 +0530

    Added code for accordian app

---
+++ src/Question.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+const Question = ({ question }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <section>
+      <div className={isOpen ? "open" : "closed"}>
+        <h4>{question.title}</h4>
+        <button onClick={() => setIsOpen(!isOpen)}>{isOpen ? "-" : "+"}</button>
+      </div>
+      {isOpen && <p>{question.info}</p>}
+    </section>
+  );
+};
+
+export default Question;
